9bf58251a7cf13fa4cd73f22c6acfaff1c49093f
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-23 02:51:29 +0530

    Update alive.py

--- MukeshRobot/modules/alive.py
+++ MukeshRobot/modules/alive.py
@@ -10,11 +10,11 @@ from telethon import __version__ as tver
 from MukeshRobot import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID,BOT_NAME,START_IMG
 
 PHOTO = [
-    "https://telegra.ph/file/d2a23fbe48129a7957887.jpg",
-    "https://telegra.ph/file/ddf30888de58d77911ee1.jpg",
-    "https://telegra.ph/file/268d66cad42dc92ec65ca.jpg",
-    "https://telegra.ph/file/13a0cbbff8f429e2c59ee.jpg",
-    "https://telegra.ph/file/bdfd86195221e979e6b20.jpg",
+    "https://te.legra.ph/file/eace7a5d8d4b515a11e50.jpg",
+    "https://te.legra.ph/file/eace7a5d8d4b515a11e50.jpg",
+    "https://te.legra.ph/file/eace7a5d8d4b515a11e50.jpg",
+    "https://te.legra.ph/file/eace7a5d8d4b515a11e50.jpg",
+    "https://te.legra.ph/file/eace7a5d8d4b515a11e50.jpg",
 ]
 
 Mukesh = [
