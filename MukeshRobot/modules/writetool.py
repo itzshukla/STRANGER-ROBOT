155d224af280371fecf2801655cfa36404e663aa
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-23 03:50:32 +0530

    Update writetool.py

--- MukeshRobot/modules/writetool.py
+++ MukeshRobot/modules/writetool.py
@@ -1,9 +1,9 @@
 """MIT License
 
-Copyright (c) 2023-24 Noob-Mukesh
+Copyright (c) 2023-24 itzshukla
 
-          GITHUB: NOOB-MUKESH
-          TELEGRAM: @MR_SUKKUN
+          GITHUB: itzshukla
+          TELEGRAM: @ITSZ_SHIVANSH
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
